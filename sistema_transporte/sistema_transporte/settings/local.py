74f0593f7675bb3995bafb14d4817893ce9520fa
estebanhirzfeld <estebanhirzfeld@gmail.com>
2023-05-10 12:16:58 -0300

    feat/fix: Conexion de nueva base de datos utilizando Postgres, agregado el uso de variables de entorno para una mayor seguridad de informacion sensible con la libreria decouple

--- sistema_transporte/sistema_transporte/settings/local.py
+++ sistema_transporte/sistema_transporte/settings/local.py
@@ -1,4 +1,6 @@
 from .base import *
+from decouple import config
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -8,14 +10,27 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'prueba',
+#         'USER': 'vortex',
+#         'PASSWORD': 'vortex.23',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'prueba',
-        'USER': 'vortex',
-        'PASSWORD': 'vortex.23',
+        'NAME': 'prueba_tecnica',
+        'USER': 'postgres',
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
+
 STATIC_URL = 'static/'
+
