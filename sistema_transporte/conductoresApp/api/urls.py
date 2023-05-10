cf9c492caa6e8ad14851f2822b08b5a13afe2289
estebanhirzfeld <estebanhirzfeld@gmail.com>
2023-05-10 12:13:50 -0300

    fix/refactor: Correccion de la impoortacion de api.api por api.views, refactorizacion del codigo para una mejor organizacion de las urls con las vistas de sus respectivas Apps

--- sistema_transporte/conductoresApp/api/urls.py
+++ sistema_transporte/conductoresApp/api/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from conductoresApp.api.api import conductor_api_view, conductor_detail_view, vehiculos_asignados, vehiculos_no_asignados
+from conductoresApp.api.views import conductor_api_view, conductor_detail_view
 
 urlpatterns = [
-    path('conductor/', conductor_api_view, name='conductores_api'),
-    path("conductor/<int:pk>/", conductor_detail_view, name="Conductor_detail_view"),
-    # path('vehiculos_asignados/<int:pk>/', vehiculos_asignados, name='vehiculos_asignados'),
-    # path('vehiculos_no_asignados/<int:pk>/', vehiculos_no_asignados, name='vehiculos_no_asigandos'),
-
+    path('', conductor_api_view, name='conductores_api'),
+    path("<int:pk>/", conductor_detail_view, name="Conductor_detail_view"),
 ]
--- sistema_transporte/pedidosApp/api/urls.py
+++ sistema_transporte/pedidosApp/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from pedidosApp.api.api import pedido_api_view, pedido_detail_view
+from pedidosApp.api.views import pedido_api_view, pedido_detail_view
 
 urlpatterns = [
-    path('pedido/', pedido_api_view, name='pedido_api'),
+    path('', pedido_api_view, name='pedido_api'),
     path("pedido/<int:pk>/", pedido_detail_view, name="pedido_detail_view")
 ]
